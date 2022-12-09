cb52ede2c1d97877ef9d2f791479ea616c4156ed
N1kl4sK <niklas.kamm@hebk.de>
2022-12-09 09:02:09 +0100

    [ADD] Anmeldung + [FIX] Fixes + [CHA] Klassendiagramm

--- src/main/java/de/hebk/menu/Menu.java
+++ src/main/java/de/hebk/menu/Menu.java
@@ -5,20 +5,11 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Menu {
-    private String path;
-
+    /**
+     * Konstruktor für das Menu
+     */
     public Menu() {
-        path = "";
-        config();
-    }
-
-    private void config() {
-        System.out.println("Geben Sie den Pfad der CSV-Datei an");
-        Scanner sc = new Scanner(System.in);
-        path = sc.nextLine();
-        CSVReader reader = new CSVReader();
-        ArrayList<String> list = reader.loadFile(path);
-        System.out.println(list);
-
+        User user = new User();
+        CSVReader csv = new CSVReader();
     }
 }
--- src/main/java/de/hebk/menu/User.java
+++ src/main/java/de/hebk/menu/User.java
@@ -1,4 +1,53 @@
 package de.hebk.menu;
 
+import java.util.Scanner;
+
 public class User {
+
+    /**
+     * Attribute
+     */
+    private String name;
+    private double punktzahl;
+
+    /**
+     * Konstruktor für User
+     */
+    public User() {
+        anmeldung();
+    }
+
+    /**
+     * Anmeldung für jeden User
+     */
+    private void anmeldung() {
+        Scanner sc = new Scanner(System.in);
+        breakLine();
+        System.out.println("Herzlich Willkommen bei Wer wird Millionär?");
+        breakLine();
+        System.out.println("Wie ist Ihr Name?");
+        breakLine();
+        this.name = sc.nextLine();
+        System.out.println("Willkommen bei Wer wird Millionär?, " + getName());
+        breakLine();
+        System.out.println("Unten links finden Sie einen Hilfe-Knopf, der Ihnen einen kurzen Überblick über das Spiel verschafft.");
+        System.out.println("Außerdem können Sie sowohl im Einzel-, als auch im Mehrspielermodus spielen.");
+        System.out.println("Um das Spiel zu starten, wählen Sie bitte einen Spielmodus aus.");
+        breakLine();
+    }
+
+    /**
+     * BreakLine Methode als optische Verbesserung
+     */
+    private void breakLine() {
+        System.out.println("----------------");
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getPunktzahl() {
+        return this.punktzahl;
+    }
 }
