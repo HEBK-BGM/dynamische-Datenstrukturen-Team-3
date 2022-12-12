c1b587e380bbb5c8db052eb11698e1680c3c67c4
maarten <maarten.fraune@hebk.de>
2022-12-12 12:28:49 +0100

    [ADD] StartGUI

---
+++ src/main/java/de/hebk/menu/GUI/StartGUI.java
@@ -0,0 +1,19 @@
+package de.hebk.menu.GUI;
+
+import javax.swing.*;
+
+public class StartGUI {
+    private JPanel rootPanel;
+    private JButton singelplayerButton;
+    private JButton button2;
+    private JButton helpButton;
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Test");
+        frame.setContentPane(new StartGUI().rootPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
